7f41bef16e479e8d42ce292eed3aa9fe7bcfaa5d
Hisoka <gintama2009@hotmail.ca>
2023-07-10 21:22:04 +0000

    bug fix attempt

--- models/brand.js
+++ models/brand.js
@@ -2,6 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const dev_db_url =
+  "mongodb+srv://admin:goblinsob9@cluster0.9icyhlu.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 const BrandSchema = new Schema({
   name: { type: String, required: true },
   cars: [{ type: Schema.Types.ObjectId, ref: "Car" }],
--- models/car.js
+++ models/car.js
@@ -2,6 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const dev_db_url =
+  "mongodb+srv://admin:goblinsob9@cluster0.9icyhlu.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 const CarSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
